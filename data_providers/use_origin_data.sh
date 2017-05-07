8d421d3e5a8a51512684f7394742d3f82f4d2fb9
Dongfeng Gu <gudongfeng@outlook.com>
2017-05-07 14:27:09 -0400

    update the dataset selection script

--- data_providers/use_origin_data.sh
+++ data_providers/use_origin_data.sh
@@ -1,4 +1,2 @@
-mv train.list train_small.list
-mv test.list test_small.list
-mv train_origin.list train.list
-mv test_origin.list test.list
+cp test_origin.list test.list
+cp train_origin.list train.list
--- data_providers/use_small_data.sh
+++ data_providers/use_small_data.sh
@@ -1,4 +1,2 @@
-mv train.list train_origin.list
-mv test.list test_origin.list
-mv train_small.list train.list
-mv test_small.list test.list
+cp test_small.list test.list
+cp train_small.list train.list
